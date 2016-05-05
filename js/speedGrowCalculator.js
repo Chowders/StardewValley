8debb509770b852ba7cbfd38b16f040b4d773cf2
David <stl_lynn@hotmail.com>
2016-05-04 22:21:22 -0500

    Updated to use more useful variable names and to pretty print to page

--- js/speedGrowCalculator.js
+++ js/speedGrowCalculator.js
@@ -1,48 +1,48 @@
-getCropData = function() {
+function getCropData() {
   $.getJSON( "json/crops.json", function( data ) {
     speedGrowCalculator(data);
   });
 };
 
-speedGrowCalculator = function(data) {
+function speedGrowCalculator(data) {
   var season = data.Spring;
   var month = 28;
   for(crop in season)
   {
     var actualCrop = season[crop];
-    minSpeed = calculateSingleGrowthTime(season[crop].growthStages,true,false,false);
-    speed = calculateSingleGrowthTime(season[crop].growthStages,true,false,true);
-    maxSpeed = calculateSingleGrowthTime(season[crop].growthStages,true,false,true);
+    speedGrowGrowthRate = calculateSingleGrowthTime(season[crop].growthStages,true,false,false);
+    speedGrowAgricultureGrowthRate = calculateSingleGrowthTime(season[crop].growthStages,true,false,true);
+    deluxeSpeedAgricultureGrowGrowthRate = calculateSingleGrowthTime(season[crop].growthStages,true,false,true);
     var daysleft, daysleftMin, daysleftMax;
     if(actualCrop.reproduceTime){
       reproduceTime = season[crop].reproduceTime;
-      minHarvest = Math.floor((month-minSpeed)/reproduceTime)+1;
-      harvest = Math.floor((month-speed)/reproduceTime)+1;
-      maxHarvest = Math.floor((month-maxSpeed)/reproduceTime)+1;
-      daysleftMin = daysLeft(month,minSpeed,minHarvest,reproduceTime);
-      daysleft = daysLeft(month,speed,harvest,reproduceTime);
-      daysleftMax = daysLeft(month,maxSpeed,maxHarvest,reproduceTime);
+      actualCrop.speedGrowHarvest = Math.floor((month-speedGrowGrowthRate)/reproduceTime)+1;
+      actualCrop.speedGrowAgricultureHarvest = Math.floor((month-speedGrowAgricultureGrowthRate)/reproduceTime)+1;
+      actualCrop.deluxeSpeedAgricultureHarvest = Math.floor((month-deluxeSpeedAgricultureGrowGrowthRate)/reproduceTime)+1;
+      actualCrop.daysleftMin = daysLeft(month,speedGrowGrowthRate,actualCrop.speedGrowHarvest,reproduceTime);
+      actualCrop.daysleft = daysLeft(month,speedGrowAgricultureGrowthRate,actualCrop.speedGrowAgricultureHarvest,reproduceTime);
+      actualCrop.daysleftMax = daysLeft(month,deluxeSpeedAgricultureGrowGrowthRate,actualCrop.deluxeSpeedAgricultureHarvest,reproduceTime);
     }else {
-      minHarvest = Math.floor(month/minSpeed);
-      harvest = Math.floor(month/speed);
-      maxHarvest = Math.floor(month/maxSpeed);
-      daysleftMin = daysLeft(month,minSpeed,minHarvest);
-      daysleft = daysLeft(month,speed,harvest);
-      daysleftMax = daysLeft(month,maxSpeed,maxHarvest);
+      actualCrop.speedGrowHarvest = Math.floor(month/speedGrowGrowthRate);
+      actualCrop.speedGrowAgricultureHarvest = Math.floor(month/speedGrowAgricultureGrowthRate);
+      actualCrop.deluxeSpeedAgricultureHarvest = Math.floor(month/deluxeSpeedAgricultureGrowGrowthRate);
+      actualCrop.daysleftMin = daysLeft(month,speedGrowGrowthRate,actualCrop.speedGrowHarvest);
+      actualCrop.daysleft = daysLeft(month,speedGrowAgricultureGrowthRate,actualCrop.speedGrowAgricultureHarvest);
+      actualCrop.daysleftMax = daysLeft(month,deluxeSpeedAgricultureGrowGrowthRate,actualCrop.deluxeSpeedAgricultureHarvest);
     }
     var normalSellPrice = actualCrop.normalSellPrice;
-    console.log(actualCrop.name);
-    console.log("G/D Minimium: " + calculateGoldPerDay(normalSellPrice,minHarvest,daysleftMin,2,month));
-    console.log("G/D Medium: " + calculateGoldPerDay(normalSellPrice,harvest,daysleft,2,month));
-    console.log("G/D Maximum: " + calculateGoldPerDay(normalSellPrice,maxHarvest,daysleftMax,2,month));
-    console.log();
-    console.log(season[crop].name + ":{");
-    console.log("Speed Grow: \n{\n Growth Time: " + minSpeed + "\n Max Harvest: " + minHarvest + "\n Days Left: " + daysleftMin + "\n}");
-    console.log("Speed Grow & Agriculturist: {\n Growth Time: " + speed + "\n Max Harvest: " + harvest + "\n Days Left: " + daysleft +"\n}");
-    console.log("Max Grow: {\n Growth Time: " + maxSpeed + "\n Max Harvest: " + maxHarvest + "\n Days Left: " + daysleftMax + "\n}\n}");
+    actualCrop.minGPD = calculateGoldPerDay(normalSellPrice,actualCrop.speedGrowHarvest,actualCrop.daysleftMin,2,month).toFixed(2);
+    actualCrop.normalGPD = calculateGoldPerDay(normalSellPrice,actualCrop.speedGrowAgricultureHarvest,actualCrop.daysleft,2,month).toFixed(2);
+    actualCrop.maxGPD = calculateGoldPerDay(normalSellPrice,actualCrop.deluxeSpeedAgricultureHarvest,actualCrop.daysleftMax,2,month).toFixed(2);
+    actualCrop.speedGrowGrowthRate = speedGrowGrowthRate;
+    actualCrop.speedGrowAgricultureGrowthRate = speedGrowAgricultureGrowthRate;
+    actualCrop.deluxeSpeedAgricultureGrowGrowthRate = deluxeSpeedAgricultureGrowGrowthRate;
   }
-};
-calculateGoldPerDay = function(sellPrice,harvest,daysLeft,minDaysLeft,month) {
+  var str = JSON.stringify(season, undefined, 4)
+  output(syntaxHighlight(str));
+}
+
+function calculateGoldPerDay(sellPrice,harvest,daysLeft,minDaysLeft,month) {
   if(daysLeft<minDaysLeft)
   {
     return (sellPrice*harvest)/month;
@@ -51,15 +51,15 @@ calculateGoldPerDay = function(sellPrice,harvest,daysLeft,minDaysLeft,month) {
   }
 }
 
-daysLeft = function(month,speed,harvest,reproduceTime) {
+function daysLeft(month,speed,harvest,reproduceTime) {
   if(reproduceTime){
     return month-speed-reproduceTime*(harvest-1);
   } else {
     return month-harvest*speed;
   }
-};
+}
 
-calculateSingleGrowthTime = function(growthStages, speedGrow, deluxeSpeedGrow, agriculturist) {
+function calculateSingleGrowthTime(growthStages, speedGrow, deluxeSpeedGrow, agriculturist) {
   if(speedGrow || deluxeSpeedGrow || agriculturist)
   {
     var totalGrowthTime = growthStages.reduce( (prev, curr) => prev + curr );
@@ -89,4 +89,28 @@ calculateSingleGrowthTime = function(growthStages, speedGrow, deluxeSpeedGrow, a
     }
     return totalGrowthTime-daysRemovable;
   }
-};
+}
+
+
+function output(inp) {
+    document.body.appendChild(document.createElement('pre')).innerHTML = inp;
+}
+
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
